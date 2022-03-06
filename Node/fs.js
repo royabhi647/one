e4f25ba51c6f3d91caae626fe96a9f917ef5b3e0
royabhi647 <royabhi647@gmail.com>
2022-03-06 09:20:48 +0530

    node started

---
+++ Node/fs.js
@@ -0,0 +1,19 @@
+// fs->File system module
+const fs=require("fs");
+console.log(fs);
+
+// append data in file
+
+let res=fs.appendFileSync("f1.txt","Hello i am f1 file");
+fs.appendFileSync("f1.txt","\nYou guys are smart");
+console.log(res);
+
+// read data from file
+
+let data=fs.readFileSync("f1.txt");
+console.log(data);
+
+console.log(data+"");
+
+// let data=fs.readFileSync("f1.txt","utf8");
+// console.log(data);
